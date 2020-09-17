1b8a64244ae8a17fa1e3d8e7fad04b361d1c4251
Robin Schultz <robin@anigenero.com>
2020-09-17 19:30:57 -0400

    groups should be either-or

--- src/auth.directive.ts
+++ src/auth.directive.ts
@@ -1,46 +1,44 @@
-import {defaultFieldResolver, GraphQLField} from 'graphql';
-import {isArray} from 'lodash';
-import {AuthenticationError} from './auth.error';
-import {User} from './user';
-import {SchemaDirectiveVisitor} from '@graphql-tools/utils';
+import { defaultFieldResolver, GraphQLField } from 'graphql';
+import { isArray } from 'lodash';
+import { AuthenticationError } from './auth.error';
+import { User } from './user';
+import { SchemaDirectiveVisitor } from '@graphql-tools/utils';
 
 export type AuthContext = { auth: User };
 export type DirectiveArgs = {
-    anonymous: boolean;
-    groups: string[];
+	anonymous: boolean;
+	groups: string[];
 };
 
 export class AuthDirective<C extends AuthContext = AuthContext> extends SchemaDirectiveVisitor {
 
-    public visitFieldDefinition(
-        _field: GraphQLField<any, AuthContext, DirectiveArgs>
-    ): GraphQLField<any, any> | void | null {
+	public visitFieldDefinition(
+		_field: GraphQLField<any, AuthContext, DirectiveArgs>
+	): GraphQLField<any, any> | void | null {
 
-        const {resolve = defaultFieldResolver} = _field;
-        const {groups} = this.args;
+		const {resolve = defaultFieldResolver} = _field;
+		const {groups} = this.args;
 
-        _field.resolve = function (...args: [any, any, AuthContext]): Promise<any> {
+		_field.resolve = function(...args: [any, any, AuthContext]): Promise<any> {
 
-            const [, , {auth}] = args;
-            if (!auth.isAnonymous()) {
+			const [, , {auth}] = args;
+			if (!auth.isAnonymous()) {
 
-                if (isArray(groups)) {
-                    groups.forEach((value: string) => {
-                        if (!auth.hasGroup(value)) {
-                            throw new AuthenticationError(`Unauthorized access for group '${value}'`);
-                        }
-                    });
-                }
+				if (isArray(groups)) {
+					if (!groups.reduce((accum, value: string) => accum || auth.hasGroup(value), false)) {
+						throw new AuthenticationError(`Unauthorized access`);
+					}
+				}
 
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-                return resolve.apply(this, args);
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+				return resolve.apply(this, args);
 
-            } else {
-                throw new AuthenticationError('Unauthorized access');
-            }
+			} else {
+				throw new AuthenticationError('Unauthorized access');
+			}
 
-        };
+		};
 
-    }
+	}
 
 }
